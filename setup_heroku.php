70c05a94676a6d37e89d4875431856c17305bcce
のぶ <nobuhiko.kimoto@gmail.com>
2018-07-28 18:16:36 +0900

    修正

--- setup_heroku.php
+++ setup_heroku.php
@@ -9,7 +9,7 @@ putenv("DBPASS=".$url['pass']);
 putenv("HTTP_URL=http://".getenv('HEROKU_APP_NAME').".herokuapp.com");
 putenv("HTTPS_URL=http://".getenv('HEROKU_APP_NAME').".herokuapp.com");
 
-// 動かない
-//exec("heroku pg:reset DATABASE --confirm ".getenv('HEROKU_APP_NAME'));
+exec("heroku pg:reset DATABASE --confirm ".getenv('HEROKU_APP_NAME'));
+exec("cp ./tests/config.php ./data/config/config.php");
 exec("sh ./eccube_install.sh heroku");
 
