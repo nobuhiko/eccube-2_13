cf2c3af4b67be1e1ee164c0a1e1f72cdc8587f92
のぶ <nobuhiko.kimoto@gmail.com>
2018-07-28 18:25:34 +0900

    修正

--- setup_heroku.php
+++ setup_heroku.php
@@ -1,15 +1,15 @@
 <?php
 $url = parse_url(getenv('DATABASE_URL'));
 
-putenv("DB=pgsql");
-putenv("DBSERVER=".$url['host']);
-putenv("DBNAME=".substr($url['path'], 1));
-putenv("USER=".$url['user']);
-putenv("DBPASS=".$url['pass']);
-putenv("HTTP_URL=http://".getenv('HEROKU_APP_NAME').".herokuapp.com");
-putenv("HTTPS_URL=http://".getenv('HEROKU_APP_NAME').".herokuapp.com");
-
 exec("heroku pg:reset DATABASE --confirm ".getenv('HEROKU_APP_NAME'));
+exec("heroku config:set DB=pgsql");
+exec("heroku config:set DBSERVER=".$url['host']);
+exec("heroku config:set DBNAME=".substr($url['path'], 1));
+exec("heroku config:set USER=".$url['user']);
+exec("heroku config:set DBPASS=".$url['pass']);
+exec("heroku config:set HTTP_URL=http://".getenv('HEROKU_APP_NAME').".herokuapp.com");
+exec("heroku config:set HTTPS_URL=https://".getenv('HEROKU_APP_NAME').".herokuapp.com");
+
 exec("cp ./tests/config.php ./data/config/config.php");
 exec("sh ./eccube_install.sh heroku");
 
