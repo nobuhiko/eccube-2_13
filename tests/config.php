88931d9878ee57b6d3ab30742503d823367c63f3
のぶ <nobuhiko.kimoto@gmail.com>
2018-07-28 19:11:05 +0900

    修正

--- tests/config.php
+++ tests/config.php
@@ -1,2 +1,11 @@
 <?php
-// 空ファイル
+$url = parse_url(getenv('DATABASE_URL'));
+
+define('ECCUBE_INSTALL', 'ON');
+define('ROOT_URLPATH', '/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'pgsql');
+define('DB_USER', $url['user']);
+define('DB_PASSWORD', $url['pass']);
+define('DB_SERVER', $url['host']);
+define('DB_NAME', substr($url['path'], 1));
