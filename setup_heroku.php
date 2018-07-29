891f3a3559b10433e8728484e4d07f1dd730ad79
のぶ <nobuhiko.kimoto@gmail.com>
2018-07-29 09:07:50 +0900

    修正

--- setup_heroku.php
+++ setup_heroku.php
@@ -4,10 +4,10 @@ $url = parse_url(getenv('DATABASE_URL'));
 $stdout= fopen('php://stdout', 'w');
 fwrite( $stdout, print_r($url)."\n" );
 
-$cmd = 'export DB_SERVER="'.$url['host'].'";';
+$cmd = 'export DBSERVER="'.$url['host'].'";';
 $cmd .= 'export DB_USER="'.$url['user'].'";';
-$cmd .= 'export DB_PASSWORD="'.$url['pass'].'";';
-$cmd .= 'export DB_NAME="'.substr($url['path'], 1).'";';
+$cmd .= 'export DBPASS="'.$url['pass'].'";';
+$cmd .= 'export DBNAME="'.substr($url['path'], 1).'";';
 
 $app = getenv('HEROKU_APP_NAME');
 $cmd .= 'export HTTP_URL="http://'.$app.'.herokuapp.com/";';
