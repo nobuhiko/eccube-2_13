e29a2f0b7c89b85cdb6ba7121f002e1132bfd94c
のぶ <nobuhiko.kimoto@gmail.com>
2018-07-29 09:01:38 +0900

    修正

--- setup_heroku.php
+++ setup_heroku.php
@@ -1,6 +1,9 @@
 <?php
 $url = parse_url(getenv('DATABASE_URL'));
 
+$stdout= fopen('php://stdout', 'w');
+fwrite( $stdout, print_r($url)."\n" );
+
 $cmd = 'export DB_SERVER="'.$url['host'].'";';
 $cmd .= 'export DB_USER="'.$url['user'].'";';
 $cmd .= 'export DB_PASSWORD="'.$url['pass'].'";';
@@ -11,4 +14,8 @@ $cmd .= 'export HTTP_URL="http://'.$app.'.herokuapp.com/";';
 $cmd .= 'export HTTPS_URL="https://'.$app.'.herokuapp.com/";';
 
 $cmd = ' sh ./eccube_install.sh heroku';
+
+$stdout= fopen('php://stdout', 'w');
+fwrite( $stdout, $cmd."\n" );
+
 echo "<pre>".shell_exec($cmd)."</pre>";
